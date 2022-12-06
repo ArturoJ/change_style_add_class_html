1ef9b1fbd339a50b4a6f76341fef2d49c3ee298b
Arturo <donarturo1986@gmail.com>
2022-12-06 21:43:22 +0000

    Update views.py

--- generador_de_clases/home/views.py
+++ generador_de_clases/home/views.py
@@ -7,77 +7,109 @@ def inicio_views(request):
 
 
 def leer_datos(request):
+    # Obtiene el contenido del campo "foo" del formulario HTML
     txt = request.POST['foo']
+    # Inicializa la variable "separar" en 2
     separar = 2
+    # Inicializa la lista vacía "lista"
     lista = []
-    texto = ''''''
+    # Inicializa la variable de texto vacía "texto"
+    texto = ''
+    # Itera sobre cada carácter en el texto
     for a in txt:
+        # Si encuentra un "<", agrega el texto actual a la lista y
+        # establece la variable "texto" en una cadena vacía
         if a == '<':
             lista.append(texto)
-            texto = ''''''
+            texto = ''
             separar = 0
+        # Si encuentra un ">", establece la variable "separar" en 1
         elif a == '>':
             separar = 1
 
+        # Si "separar" es 2, agrega el carácter actual al texto
         if separar == 2:
             texto += a
 
+        # Si "separar" es 1, agrega el carácter actual al texto y
+        # luego agrega el texto actual a la lista y establece la
+        # variable "texto" en una cadena vacía
         if separar == 1:
             texto += a
             lista.append(texto)
-            texto = ''''''
+            texto = ''
             separar = 2
 
+        # Si "separar" es 0, agrega el carácter actual al texto
         if separar == 0:
             texto += a
 
+    # Filtra la lista para eliminar elementos que contengan solo espacios en blanco
     filtered = filter(lambda x: not re.match(r'^\s*$', x), lista)
-    texto_para_text_area = ''''''
+    # Inicializa la variable de texto vacía "texto_para_text_area"
+    texto_para_text_area = ''
+    # Itera sobre cada elemento filtrado en la lista
     for a in filtered:
-        texto_para_text_area += a+'\n'
+        # Agrega el elemento actual al texto final, seguido de un salto de línea
+        texto_para_text_area += a + '\n'
 
+    # Devuelve el texto final como respuesta HTTP
     return HttpResponse(texto_para_text_area)
 
 
-
-new_class = ''''''
 def separa_styles(request):
+    # Define la variable global "new_class"
     global new_class
+    # Obtiene el contenido del campo "foo2" del formulario HTML
     txt = request.POST['foo2']
+    # Inicializa la variable "separar" en 2
     separar = 2
+    # Inicializa la lista vacía "lista"
     lista = []
-    texto = ''''''
+    # Inicializa la variable de texto vacía "texto"
+    texto = ''
+    # Itera sobre cada carácter en el texto
     for a in txt:
-
+        # Si encuentra un "<", agrega el texto actual a la lista y
+        # establece la variable "texto" en una cadena vacía
         if a == '<':
             lista.append(texto)
-            texto = ''''''
+            texto = ''
             separar = 0
+        # Si encuentra un ">", establece la variable "separar" en 1
         elif a == '>':
             separar = 1
 
+        # Si "separar" es 2, agrega el carácter actual al texto
         if separar == 2:
             texto += a
 
+        # Si "separar" es 1, agrega el carácter actual al texto y
+        # luego agrega el texto actual a la lista y establece la
+        # variable "texto" en una cadena vacía
         if separar == 1:
             texto += a
             lista.append(texto)
-            texto = ''''''
+            texto = ''
             separar = 2
 
+        # Si "separar" es 0, agrega el carácter actual al texto
         if separar == 0:
             texto += a
 
+    # Filtra la lista para eliminar elementos que contengan solo espacios en blanco
     filtered = filter(lambda x: not re.match(r'^\s*$', x), lista)
-    texto_para_text_area = ''''''
+    # Inicializa la variable de texto vacía "texto_para_text_area"
+    texto_para_text_area = ''
+    # Inicializa la variable "class_add" en 0
     class_add = 0
 
-
+    # Itera sobre cada elemento filtrado en la lista
     for a in filtered:
+        # Incrementa la variable "class_add" en 1
         class_add += 1
-        #Solo los que tengan style y class
-        if a.find('class') != -1 and a.find('style=') != -1:
-
+        # Si el elemento contiene "class" y "style=", realiza las siguientes operaciones:
+        if a.find('class') != -1 and a.find('style=') !=
 
             # Busca style y lo separa para mostrar en css
             select_style = re.compile('style=".*?"')
